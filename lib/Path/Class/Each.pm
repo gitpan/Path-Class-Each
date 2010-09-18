ec1693ce9f370d6cdeec5ae513994a4b378b23c7
Andy Armstrong <andy@hexten.net>
2010-09-18 14:18:58 +0000

    Import of ANDYA/Path-Class-Each-0.02 from CPAN.
    gitpan-cpan-distribution: Path-Class-Each
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ANDYA/Path-Class-Each-0.02.tar.gz
    gitpan-cpan-author:       ANDYA
    gitpan-cpan-maturity:     released

--- lib/Path/Class/Each.pm
+++ lib/Path/Class/Each.pm
@@ -6,7 +6,7 @@ use strict;
 use Carp qw( croak );
 use Path::Class;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 NAME
 
@@ -14,12 +14,14 @@ Path::Class::Each - Iterate lines in a file
 
 =head1 VERSION
 
-This document describes Path::Class::Each version 0.01
+This document describes Path::Class::Each version 0.02
 
 =head1 SYNOPSIS
 
   use Path::Class;
   use Path::Class::Each;
+
+  ## Files ##
 
   # Iterator interface
   my $iter = file( 'foo', 'bar' )->iterator;
@@ -40,19 +42,50 @@ This document describes Path::Class::Each version 0.01
     }
   );
 
+  ## Directories ##
+  
+  # Iterator interface
+  my $iter = dir( 'foo', 'bar' )->iterator;
+  while ( defined( my $file = $iter->() ) ) {
+    print "File: $file\n";
+  }
+
+  # 'next' interface
+  my $file = dir( 'foo', 'bar' );
+  while ( defined( my $file = $dir->next_file ) ) {
+    print "File: $file\n";
+  }
+
+  # Callback interface
+  dir( 'foo', 'bar' )->each(
+    sub {
+      print "File: $_\n";
+    }
+  );
+
 =head1 DESCRIPTION
 
-C<Path::Class::Each> augments L<Path::Class::File> to provide three different
-ways of iterating over the lines of a file.
+C<Path::Class::Each> augments L<Path::Class::File> and
+L<Path::Class::Dir> to provide three different ways of iterating over
+the lines of a file and three ways of iterating the files recursively
+contained in a directory.
 
 C<Path::Class::File> provides a C<slurp> method that returns the
 contents of a file (either as a scalar or an array) but has no support
 for reading a file a line at a time. For large files it may be desirable
 to iterate through the lines; that's where this module comes in.
 
+C<Path::Class::Dir> provides C<children> which returns the files and
+directories immediately contained in a directory but does not expose a
+'find' interface to recursively search a directory. This module provides
+iterators that visit all the files in the directory tree below a
+directory.
+
 =head1 INTERFACE
 
-=head2 C<< Path::Class::File->iterator >>
+=head2 File Iterators
+
+=head3 C<< Path::Class::File->iterator >>
 
 Get an iterator that returns the lines in a file. Returns C<undef> when
 there are no more lines to return.
@@ -98,7 +131,7 @@ sub Path::Class::File::iterator {
   my $mode  = delete $opt{iomode};
   my $chomp = delete $opt{chomp};
 
-  croak "unknown options: ", join ', ', sort keys %opt
+  croak "Unknown options: ", join ', ', sort keys %opt
    if keys %opt;
 
   my $fh = $self->open( $mode ) or croak "Can't read $self: $!\n";
@@ -110,7 +143,7 @@ sub Path::Class::File::iterator {
   };
 }
 
-=head2 C<< Path::Class::File->next >>
+=head3 C<< Path::Class::File->next >>
 
 Return the next line from a file. Returns C<undef> when all lines have
 been read.
@@ -123,7 +156,7 @@ The same options that L<iterator> accepts may be passed to C<next>:
     print "Line: $line\n";
   }
 
-=head3 NOTE
+=head4 NOTE
 
 It may be tempting to use an idiom like:
 
@@ -136,20 +169,7 @@ That will create a new C<Path::Class::File> and, therefore, a new
 iterator each time it is called with the result that the first line of
 the file will be returned repeatedly.
 
-=cut
-
-sub Path::Class::File::next {
-  my $self = shift;
-
-  $self->{_iter} = $self->iterator( @_ )
-   unless $self->{_iter};
-
-  my $line = $self->{_iter}->();
-  delete $self->{_iter} unless defined $line;
-  return $line;
-}
-
-=head2 C<< Path::Class::File->each >>
+=head3 C<< Path::Class::File->each >>
 
 Call a supplied callback for each line in a file. The same options that
 L<iterator> accepts may be passed:
@@ -158,16 +178,77 @@ L<iterator> accepts may be passed:
 
 Within the callback the current line will be in C<$_>.
 
+=head2 Directory Iterators
+
+=head3 C<< Path::Class::Dir->iterator >>
+
+Return an iterator that returns each of the files in and below a
+directory.
+
 =cut
 
-sub Path::Class::File::each {
-  my $self = shift;
-  my @opt  = @_;
-  my $cb   = pop @opt;
+sub Path::Class::Dir::iterator {
+  my ( $self, @opt ) = @_;
 
-  my $iter = $self->iterator( @opt );
-  while ( defined( local $_ = $iter->() ) ) {
-    $cb->();
+  croak "each requires a number of name => value options"
+   if @opt % 2;
+
+  my @queue = $self->children( @opt );
+  return sub {
+    TRY: {
+      return unless @queue;
+      my $obj = shift @queue;
+      if ( $obj->isa( 'Path::Class::Dir' ) ) {
+        unshift @queue, $obj->children( @opt );
+        redo TRY;
+      }
+      return $obj;
+    }
+  };
+}
+
+=head3 C<< Path::Class::Dir->next_file >>
+
+Return the next file from a recursive search of a directory. Returns
+C<undef> when all lines have been read.
+
+Internally L<iterator> is called if necessary to create a new iterator.
+The same options that L<iterator> accepts may be passed to C<next_file>:
+
+  my $dir = dir( 'foo', 'bar' );
+  while ( defined( my $file = $dir->next_file ) ) {
+    print "File: $file\n";
+  }
+
+=head3 C<< Path::Class::Dir->each >>
+
+Call a supplied callback for each file in a directory. The same options
+that L<iterator> accepts may be passed:
+
+  dir( 'foo', 'bar' )->each( sub { print "File: $_\n" } );
+
+Within the callback the current file will be in C<$_>.
+
+=cut
+
+BEGIN {
+  my @extend = qw( Path::Class::File Path::Class::Dir );
+  for my $class ( @extend ) {
+    no strict 'refs';
+    *{"${class}::each"} = sub {
+      my ( $self, @opt ) = @_;
+      my $cb   = pop @opt;
+      my $iter = $self->iterator( @opt );
+      while ( defined( local $_ = $iter->() ) ) { $cb->() }
+    };
+    my $next = $class eq 'Path::Class::Dir' ? 'next_file' : 'next';
+    *{"${class}::${next}"} = sub {
+      my $self = shift;
+      $self->{_iter} = $self->iterator( @_ ) unless $self->{_iter};
+      my $line = $self->{_iter}->();
+      delete $self->{_iter} unless defined $line;
+      return $line;
+    };
   }
 }
 
@@ -176,6 +257,7 @@ __END__
 
 =head1 DEPENDENCIES
 
+L<Path::Class>
 
 =head1 AUTHOR
 
